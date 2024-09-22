95a0d87d7e49cc4c0d1f9dafe6f517323f51bdb7
Elvis <oklosamuel50@gmail.com>
2024-09-22 14:09:22 +0100

    send them all

--- run.py
+++ run.py
@@ -1,9 +1,31 @@
+import requests
 from dotenv import load_dotenv
+from apscheduler.schedulers.background import BackgroundScheduler
 from WQMS import app
+from WQMS.scheduler import scheduler
+#from WQMS.routes import routes
+#from WQMS.routes2 import routes2
 
-
+# Load environment variables
 load_dotenv()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# Register blueprints
+#app.register_blueprint(routes)
+#app.register_blueprint(routes2)
 
+def scheduled_task():
+    try:
+        response = requests.get('https://wqms-kwv1.onrender.com/run-model')
+        print(response.json())
+    except Exception as e:
+        print(f"Failed to trigger script: {e}")
+
+scheduler = BackgroundScheduler()
+scheduler.add_job(scheduled_task, 'interval', minutes=3)
+scheduler.start()
+
+if __name__ == '__main__':
+    try:
+        app.run(host='0.0.0.0', port=5000)#change to debug=True while deploying live
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
